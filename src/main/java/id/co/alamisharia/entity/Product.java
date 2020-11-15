0197aeb9cc4ae9a2932c1788871cd9c82f8f708f
Jaka Ardita <jakaardita@gmail.com>
2020-11-15 21:31:17 +0700

    add annotation for orm

--- src/main/java/id/co/alamisharia/entity/Product.java
+++ src/main/java/id/co/alamisharia/entity/Product.java
@@ -1,6 +1,19 @@
 package id.co.alamisharia.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCT_TBL")
 public class Product {
+    @Id
+    @GeneratedValue
     private int id;
     private int sellerId;
     private String nama;
--- src/main/java/id/co/alamisharia/entity/Seller.java
+++ src/main/java/id/co/alamisharia/entity/Seller.java
@@ -1,6 +1,22 @@
 package id.co.alamisharia.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "SELLER_TBL")
 public class Seller {
+    @Id
+    @GeneratedValue
     private int id;
     private String nama;
     private String kota;
