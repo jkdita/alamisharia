708627296de2ddbea36e7e60ccc59d692b5177e1
Jaka Ardita <jakaardita@gmail.com>
2020-11-15 21:22:59 +0700

    add product entity

---
+++ src/main/java/id/co/alamisharia/entity/Product.java
@@ -0,0 +1,9 @@
+package id.co.alamisharia.entity;
+
+public class Product {
+    private int id;
+    private int sellerId;
+    private String nama;
+    private String satuan;
+    private double hargaSatuan;
+}
