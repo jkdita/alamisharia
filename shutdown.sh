8c290d270f3adca057082eb7f356a83efdb14eea
Jaka Ardita <jakaardita@gmail.com>
2020-11-16 02:29:16 +0700

    add deployment scripts

---
+++ shutdown.sh
@@ -0,0 +1,10 @@
+curl -X POST localhost:8080/shutdown
+sleep 5
+PID=$(ps -eo pid,command | grep alamisharia-rest-api-0.0.1-SNAPSHOT.jar| grep -v grep | awk '{print $1}')
+if [ -z $PID ];
+then
+echo "alamisharia rest api is stopped"
+else
+echo "alamisharia rest api is still running, we will force to kill it"
+for pid in $(ps -ef | grep "alamisharia-rest-api" | awk '{print $2}'); do kill -9 $pid; done
+fi
---
+++ start.sh
@@ -0,0 +1,4 @@
+JAVA_OPTS="-Duser.timezone=Asia/Jakarta -jar"
+JAR_FILE="target/alamisharia-rest-api-0.0.1-SNAPSHOT.jar"
+java $JAVA_OPTS $JAR_FILE &
+exit 0
