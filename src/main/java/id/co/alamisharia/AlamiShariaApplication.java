fd6f7129099221a0c735074a52e5c8a5142058ac
Jaka Ardita <jakaardita@gmail.com>
2020-11-15 21:12:30 +0700

    init maven project

---
+++ src/main/java/id/co/alamisharia/AlamiShariaApplication.java
@@ -0,0 +1,13 @@
+package id.co.alamisharia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlamiShariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlamiShariaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/co/alamisharia/AlamiShariaApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.alamisharia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlamiShariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
